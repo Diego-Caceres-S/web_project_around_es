9c3c82163cd768e5603fc5e601b2d5a6844247f0
diego-caceres <diego.cs4@hotmail.com>
2026-02-21 13:43:07 -0500

    feat: agregar funciones modal edit profile

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,51 @@ const initialCards = [
 initialCards.forEach(function (element) {
   console.log(element.name + ".");
 });
+
+const modalEditProfile = document.querySelector("#edit-popup");
+const buttonEditOpen = document.querySelector(".profile__edit-button");
+const buttonEditClose = modalEditProfile.querySelector(".popup__close");
+const titleProfile = document.querySelector(".profile__title");
+const descriptionProfile = document.querySelector(".profile__description");
+const formEditProfile = document.querySelector("#edit-profile-form");
+let nameInput = modalEditProfile.querySelector(".popup__input_type_name");
+let descriptionInput = modalEditProfile.querySelector(
+  ".popup__input_type_description",
+);
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  nameInput.value = titleProfile.textContent;
+  descriptionInput.value = descriptionProfile.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(modalEditProfile);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  titleProfile.textContent = nameInput.value;
+  descriptionProfile.textContent = descriptionInput.value;
+}
+
+buttonEditOpen.addEventListener("click", function () {
+  handleOpenEditModal();
+});
+
+buttonEditClose.addEventListener("click", function () {
+  closeModal(modalEditProfile);
+});
+
+formEditProfile.addEventListener("submit", function (evt) {
+  handleProfileFormSubmit(evt);
+  closeModal(modalEditProfile);
+});
